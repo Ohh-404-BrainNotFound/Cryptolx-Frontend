107734f710f239d525a8da6d9f2c8f1241b1daa8
nlok5923 <nlokhande5923@gmail.com>
2021-09-09 16:38:13 +0530

    adding navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Menu, Dropdown, Form, Button, Icon } from "semantic-ui-react";
+function Navbar(){
+    return (
+        <div className="navbar">
+           <Menu >
+            </Menu> 
+        </div>
+    );
+
+}
+export default Navbar;
