4fa32f64d7fc80992c2a8143a5151f80e771f651
Naman-1234 <namankalrabhiwani54@gmail.com>
2021-09-20 17:58:03 +0530

    Made footer resemblance similar to design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,36 +1,43 @@
 import React from 'react';
 import { Segment, Container, Grid, Header, List } from 'semantic-ui-react';
+import './Footer.scss';
 function Footer() {
   return (
     <div className='footer'>
-      <Segment>
-        <Container>
+      <Segment className='footer_segment center aligned  '>
+        <Container className='footer_container'>
           <Grid>
-            <Grid.Column width={6}>
+            <Grid.Column width={6} className='left aligned'>
               <Header as='h1' content='CRYPTOLX'></Header>
-              <List>
+              <List className='animated'>
                 <List.Item as='span'>2719 Bel Meadow Drive</List.Item>
                 <List.Item as='span'>Los Angeles CA</List.Item>
                 <List.Item as='span'>90017</List.Item>
                 <List.Item as='span'>900-309-6214</List.Item>
               </List>
             </Grid.Column>
-            <Grid.Column width={2}>
-              <List>
+            <Grid.Column width={3}>
+              <Header className='hidden'>I am Hidden</Header>
+              <List className='animated'>
+                {/* <List.Item className='hidden'>I am Hidden</List.Item> */}
                 <List.Item as='span'>Buy</List.Item>
                 <List.Item as='span'>Sell</List.Item>
                 <List.Item as='span'>About Us</List.Item>
               </List>
             </Grid.Column>
-            <Grid.Column width={2}>
-              <List>
+            <Grid.Column width={3}>
+              <Header className='hidden'>I am Hidden</Header>
+              <List className='animated'>
+                {/* <List.Item className='hidden'>I am Hidden</List.Item> */}
                 <List.Item as='span'>Privacy</List.Item>
                 <List.Item as='span'>Terms of Use</List.Item>
                 <List.Item as='span'>Crypto</List.Item>
               </List>
             </Grid.Column>
-            <Grid.Column width={2}>
-              <List>
+            <Grid.Column width={3}>
+              <Header className='hidden'>I am Hidden</Header>
+              <List className='animated'>
+                {/* <List.Item className='hidden'>I am Hidden</List.Item> */}
                 <List.Item as='a'>Facebook</List.Item>
                 <List.Item as='a'>Twitter</List.Item>
                 <List.Item as='a'>Instagram</List.Item>
